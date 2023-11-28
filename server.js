5a9827e1ae612cc9ee776bf43139091f6046c226
lanbow93 <the.doctor1501@gmail.com>
2023-11-28 08:50:21 -0600

    added deployed site to cors list

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173']
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://joelbaycollections.netlify.app']
 }));
 app.use(express.json());
 app.use(morgan("tiny"));
