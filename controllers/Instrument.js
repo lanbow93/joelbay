cbb8b20549f93631de71b063f43ae0457e092f34
lanbow93 <the.doctor1501@gmail.com>
2023-11-29 13:24:05 -0600

    with multer image upload works

--- controllers/Instrument.js
+++ controllers/Instrument.js
@@ -1,5 +1,8 @@
 import express from 'express';
 const router = express.Router();
+import multer from 'multer'
+const storage = multer.memoryStorage(); // Store the file in memory
+const upload = multer({ storage: storage });
 import Instrument from '../models/Instrument.js';
 import fetch from 'node-fetch';
 import adminAuth from '../middleware/adminAuth.js';
@@ -30,63 +33,72 @@ router.delete("/:id", async(request, response) => {
     }
 })
 
-// Creates a new instrument
-router.post("/", async (request, response) => {
-  
-    try {
-      // Assuming request.body.image is a File object (uploaded file)
-      const image = request.body.image;
-  
-      // Upload image to Imgur
-      const imgurApiResponse = await uploadImageToImgur(image);
-      console.log(imgurApiResponse)
-  
-      // Check if Imgur upload was successful
-      
-      if (imgurApiResponse && imgurApiResponse.data && imgurApiResponse.data.link) {
-        // Update the database with the Imgur image link
-        const newInstrument = await Instrument.create({
-          name: request.body.name,
-          description: request.body.description,
-          image: imgurApiResponse.data.link, // Update image to Imgur link
-          price: request.body.price,
-          quantityAvailable: request.body.quantityAvailable,
-          brand: request.body.brand,
-          category: request.body.category,
-          condition: request.body.condition,
-        });
-  
-        if (newInstrument) {
-          response.status(200).json(newInstrument);
-        } else {
-          response.status(400).json({ error: "Unable to create record", data: newInstrument });
-        }
+// Creates a new instrument with multer middleware for handling file upload
+router.post("/", upload.single('image'), async (request, response) => {
+  try {
+    // Assuming request.file is the uploaded file
+    const image = request.file;
+    
+
+    // Upload image to Imgur
+    const imgurApiResponse = await uploadImageToImgur(image.buffer);
+    console.log(imgurApiResponse);
+
+    // Check if Imgur upload was successful
+    if (imgurApiResponse && imgurApiResponse.data && imgurApiResponse.data.link) {
+      // Update the database with the Imgur image link
+      const newInstrument = await Instrument.create({
+        name: request.body.name,
+        description: request.body.description,
+        imageUrl: imgurApiResponse.data.link , // Update image to Imgur link
+        price: request.body.price,
+        quantityAvailable: request.body.quantityAvailable,
+        brand: request.body.brand,
+        category: request.body.category,
+        condition: request.body.condition,
+      });
+
+      if (newInstrument) {
+        response.status(200).json(newInstrument);
       } else {
-        response.status(400).json({ error: "Imgur image upload failed", data: imgurApiResponse });
+        response.status(400).json({ error: "Unable to create record", data: newInstrument });
       }
-    } catch (error) {
-      response.status(400).json(error);
+    } else {
+      response.status(400).json({ error: "Imgur image upload failed", data: imgurApiResponse });
     }
-  });
+  } catch (error) {
+    console.log(error);
+    response.status(400).json(error);
+  }
+});
   
   // Function to upload an image to Imgur
   async function uploadImageToImgur(image) {
-    const imgurApiUrl = 'https://api.imgur.com/3/image';
-    const imgurClientId = process.env.CLIENTID; // Replace with your Imgur client ID
+  const imgurApiUrl = `https://api.imgur.com/3/image`;
+  const imgurClientId = process.env.CLIENTID;
 
-    try{
-      const response = await fetch(imgurApiUrl, {
-        method: 'POST',
-        headers: {
-          Authorization: `Client-ID ${imgurClientId}`,
-        },
-      body: formData,
+  try {
+
+    const response = await fetch(imgurApiUrl, {
+      method: 'POST',
+      headers: {
+        Authorization: `Client-ID ${imgurClientId}`,
+      },
+      body: image,
     });
-    return response.json();
-  } catch(error){
-      return error
+
+    if (response.ok) {
+      return response.json();
+    } else {
+      const errorResponse = await response.json(); // Attempt to parse error response as JSON
+      console.error('Imgur API Error:', errorResponse);
+      return { error: 'Imgur API Error', data: errorResponse };
     }
+  } catch (error) {
+    console.error('Error during Imgur API request:', error);
+    return { error: 'Imgur API Request Error', data: null };
   }
+}
 
 
 // Updates an instrument
